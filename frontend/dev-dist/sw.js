d181cfcaf7da47d9782c51c5967e0ddb2ba234e4
leewooseong <sitpo96@naver.com>
2024-06-10 17:25:23 +0900

    ReactQueryDevtools로 인한 build 에러 해결
    Change-Id: I98d165c187b42ce7b7f5bc14e4ceb38c5aec0ff6

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g5ppbfevip"
+    "revision": "0.lcl58g364a8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -102,7 +102,7 @@ function App() {
 		<ThemeProvider theme={theme}>
 			<ToastContainer position="top-center" />
 			<RouterProvider router={router} />
-			{/* <ReactQueryDevtools /> */}
+			<ReactQueryDevtools />
 		</ThemeProvider>
 	)
 }
