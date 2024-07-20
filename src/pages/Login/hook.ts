ef40e8128deeff355423c11a2c14fa6c871a13f8
이우성 <42796944+leewooseong@users.noreply.github.com>
2024-07-20 17:33:57 +0900

    Fix: 계정이 있어도 join 페이지로 이동되는 문제 수정

--- src/pages/Login/hook.ts
+++ src/pages/Login/hook.ts
@@ -12,21 +12,21 @@ export const useDoLogin = () => {
 		// 1. 로그인 api 요청(browser -> spring server)이 성공하면, access & refresh token이 담기게 된다.
 		const loginRes = await getToken(loginType, code)
 
-		// 2. 로그인이 성공하면 회원가입된 유저인지 판별
 		if (loginRes.kakaoLoginResult === 'fail') {
 			alert('로그인 실패')
 			return
 		}
 
+		// 2. 로그인이 성공하면 회원가입된 유저인지 판별
 		// 기존 유저라면 메인 페이지로 이동한다.
+		// 회원가입이 필요한 유저라면 카카오 닉네임을 joinInfoStore에 저장하고 가입 페이지로 이동한다.
 		if (loginRes.hasAccount) {
 			routeTo('/')
+		} else {
+			setNickname(loginRes.nickname)
+			setIsInitialNicknameDuplicated(loginRes.nicknameDuplicate)
+			routeTo('/join')
 		}
-
-		// 회원가입이 필요한 유저라면 카카오 닉네임을 joinInfoStore에 저장하고 가입 페이지로 이동한다.
-		setNickname(loginRes.nickname)
-		setIsInitialNicknameDuplicated(loginRes.nicknameDuplicate)
-		routeTo('/join')
 	}
 
 	return { doLogin }
